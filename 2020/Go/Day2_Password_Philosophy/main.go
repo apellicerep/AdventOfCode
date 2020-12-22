8c704ee838f1cacfd300644131f60a3134dc2041
arpel4 <arturo.pellicer@ingka.com>
2020-12-22 11:14:21 +0100

    Day2_Password_Philosophy - Refactor

--- 2020/Go/Day2_Password_Philosophy/main.go
+++ 2020/Go/Day2_Password_Philosophy/main.go
@@ -10,12 +10,10 @@ import (
 
 //example  8-11 c: cbccrbdwdccccsk
 func main() {
-	//part1()
-	part2()
-
+	miFunc()
 }
 
-func part1() {
+func miFunc() {
 	file, err := os.Open("/Users/arpel4/Documents/Go/AdventOfCode/2020/Go/Day2_Password_Philosophy/input2.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -23,7 +21,9 @@ func part1() {
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	var cntOk int
+
+	var cntOk1 int
+	var cntOk2 int
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -33,43 +33,31 @@ func part1() {
 		max, _ := strconv.Atoi(lineArray[0][index+1:])
 		word := string(lineArray[1][0])
 		password := lineArray[2]
-		charCount := make(map[string]int)
-		for _, v := range password {
-			charCount[string(v)]++
-		}
-		if v, ok := charCount[word]; ok {
-			if v >= min && v <= max {
-				cntOk++
-			}
-		}
+		part1(password, word, min, max, &cntOk1)
+		part2(password, word, min, max, &cntOk2)
+
 	}
-	fmt.Println(cntOk)
+	fmt.Println(cntOk1)
+	fmt.Println(cntOk2)
 
 }
 
-func part2() {
-	file, err := os.Open("/Users/arpel4/Documents/Go/AdventOfCode/2020/Go/Day2_Password_Philosophy/input2.txt")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
+func part1(p, w string, min, max int, c *int) {
+	charCount := make(map[string]int)
+	for _, v := range p {
+		charCount[string(v)]++
 	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	var cntOk int
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		lineArray := strings.Split(line, " ")
-		index := strings.Index(lineArray[0], "-")
-		pos1, _ := strconv.Atoi(lineArray[0][0:index])
-		pos2, _ := strconv.Atoi(lineArray[0][index+1:])
-		word := string(lineArray[1][0])
-		password := lineArray[2]
-		charPos1 := string(password[pos1-1])
-		charPos2 := string(password[pos2-1])
-		if (charPos1 == word) != (charPos2 == word) {
-			cntOk++
+	if v, ok := charCount[w]; ok {
+		if v >= min && v <= max {
+			*c++
 		}
 	}
-	fmt.Println(cntOk)
+}
+
+func part2(p, w string, min, max int, c *int) {
+	charPos1 := string(p[min-1])
+	charPos2 := string(p[max-1])
+	if (charPos1 == w) != (charPos2 == w) {
+		*c++
+	}
 }
